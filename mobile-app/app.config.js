8119ecc682d960890e157c6a5fde8776736196bd
shreyansh-618 <chaudharyshreyash618@gmail.com>
2026-03-29 12:42:44 +0530

    refactor: improve config management by migrating to app.config.js and removing hardcoded Firebase credentials in favor of environment variables

---
+++ mobile-app/app.config.js
@@ -0,0 +1,59 @@
+export default {
+  expo: {
+    name: "AcadHub",
+    slug: "acadhub-mobile",
+    version: "1.0.0",
+    scheme: "acadhub",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      supportsTabletMode: true,
+      bundleIdentifier: "com.acadhub.mobile",
+      usesNonExemptEncryption: false,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      package: "com.acadhub.mobile",
+      softwareKeyboardLayoutMode: "pan",
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    plugins: [
+      [
+        "expo-build-properties",
+        {
+          ios: {
+            deploymentTarget: "15.1",
+          },
+          android: {
+            minSdkVersion: 24,
+            targetSdkVersion: 36,
+            compileSdkVersion: 36,
+          },
+        },
+      ],
+    ],
+    extra: {
+      eas: {
+        projectId: "acadhub-mobile-project",
+      },
+      firebaseApiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId:
+        process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    },
+  },
+};
